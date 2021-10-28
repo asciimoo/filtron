cfc85d516a61165342d4ca4da7086b037d000cba
Alexandre Flament <alex@al-f.net>
2021-10-28 11:13:01 +0200

    [mod] allow rules without stop without action or subrule
    Related: https://github.com/dalf/filtron/pull/2

--- rule/rule.go
+++ rule/rule.go
@@ -114,8 +114,8 @@ func ParseJSON(jsonData []byte) ([]*Rule, error) {
 func (r *Rule) Init() error {
 	r.filterMatchCount = 0
 	r.lastTick = uint64(time.Now().Unix())
-	if len(r.RawActions) == 0 && len(r.SubRules) == 0 {
-		return errors.New(fmt.Sprintf("At least one subrule or action required in rule: %q", r.Name))
+	if len(r.RawActions) == 0 && len(r.SubRules) == 0 && (!r.Stop) {
+		return errors.New(fmt.Sprintf("At least one subrule or action or stop required in rule: %q", r.Name))
 	}
 	if err := r.ParseFilters(r.RawFilters); err != nil {
 		return err
